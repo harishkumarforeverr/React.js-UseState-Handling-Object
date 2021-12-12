951404806e3ab43d928be29a91f5930d9024fc6a
harishkumarforever <h.nunavath@dev2prod.com>
2021-12-12 08:13:42 -0500

    initital commit

---
+++ build/precache-manifest.17177e827e99f54ce74d106d43a1f41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09c4f7ae68fe00628f7b3a398eaf6d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6d42bb529fc78258cf",
+    "url": "/static/css/main.842ff858.chunk.css"
+  },
+  {
+    "revision": "dc1b8ef01734aeeb1da6",
+    "url": "/static/js/2.f82db772.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f82db772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6d42bb529fc78258cf",
+    "url": "/static/js/main.e174a947.chunk.js"
+  },
+  {
+    "revision": "c0a1fb1ae4be33396d0d",
+    "url": "/static/js/runtime-main.e8e169bd.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,3 @@
+i came to know that if we want to preserver the previous State value in the React State
+we need to Speard all the old State values when we are setting the new State
+so that new State and old State willl be Stay in the Object
